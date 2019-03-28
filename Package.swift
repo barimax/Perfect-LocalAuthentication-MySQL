4f070a489d90d8f555ca249b10dc9908ab1d72ee
barimax <georgie@barimax.bg>
2019-03-28 10:49:16 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Perfect-LocalAuthentication-MySQL",
-    targets: [
-		Target(
-			name: "PerfectLocalAuthentication",
-			dependencies: []
-		)
-		],
-    dependencies: [
-		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-    	.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3)
+	name: "Perfect-LocalAuthentication-MySQL",
+	products: [
+		.library(name: "PerfectLocalAuthentication", targets: ["PerfectLocalAuthentication"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/iamjono/JSONConfig.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", from: "3.0.0"),
+		.package(url: "https://github.com/SwiftORM/MySQL-StORM.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
+    	.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", from: "3.0.0")
+	],
+	targets: [
+		.target(name: "PerfectLocalAuthentication", dependencies: ["JSONConfig", "PerfectRequestLogger","PerfectSMTP","MySQLStORM","PerfectSessionMySQL","PerfectMustache","PerfectHTTP"])
 	]
 )
+
